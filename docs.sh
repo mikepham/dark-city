e7234125a23046e5aa8c68f4963012f848cd2a7f
Mike Pham <opensource@nativecode.com>
2019-01-12 00:33:53 -0500

    changing filenames

--- docs.sh
+++ docs.sh
@@ -5,16 +5,16 @@ if [ $# -eq 0 ]; then
   exit
 fi
 
-echo "#" > ./env_$1/MODULES.md
+echo "#" > ./env_$1/README.md
 
-echo "## Module: environment" >> ./env_$1/MODULES.md
-terraform-docs markdown environment >> ./env_$1/MODULES.md
+echo "## Module: environment" >> ./env_$1/README.md
+terraform-docs markdown environment >> ./env_$1/README.md
 
-echo "## Module: certificates" >> ./env_$1/MODULES.md
-terraform-docs markdown modules/certificates >> ./env_$1/MODULES.md
+echo "## Module: certificates" >> ./env_$1/README.md
+terraform-docs markdown modules/certificates >> ./env_$1/README.md
 
-echo "## Module: rancher" >> ./env_$1/MODULES.md
-terraform-docs markdown modules/rancher >> ./env_$1/MODULES.md
+echo "## Module: rancher" >> ./env_$1/README.md
+terraform-docs markdown modules/rancher >> ./env_$1/README.md
 
-echo "## Module: rancher-aws" >> ./env_$1/MODULES.md
-terraform-docs markdown modules/secrets >> ./env_$1/MODULES.md
+echo "## Module: rancher-aws" >> ./env_$1/README.md
+terraform-docs markdown modules/secrets >> ./env_$1/README.md
