8472e2a690a2eb4c98305ae155c26065fba7f4a7
Mike Pham <opensource@nativecode.com>
2019-01-18 18:05:15 -0500

    adding aws efs support

---
+++ modules/aws/efs/output.tf
@@ -0,0 +1,21 @@
+output "efs_arn" {
+  description = "EFS ARN"
+  depends_on  = ["${aws_efs_file_system.nfs.*}"]
+  value       = "${aws_efs_file_system.nfs.*.arn}"
+}
+
+output "efs_dns_name" {
+  description = "EFS DNS Name"
+  depends_on  = ["${aws_efs_file_system.nfs.*}"]
+  value       = "${aws_efs_file_system.nfs.*.dns_name}"
+}
+
+output "efs_id" {
+  description = "EFS ID"
+  depends_on  = ["${aws_efs_file_system.nfs.*}"]
+  value       = "${aws_efs_file_system.nfs.*.id}"
+}
+
+output "efs_security_group" {
+  value = "${aws_security_group.nfs.*.id}"
+}
--- modules/aws/efs/variables.tf
+++ modules/aws/efs/variables.tf
@@ -3,7 +3,27 @@ variable "domain" {
   type        = "string"
 }
 
-variable "environment" {
-  description = "Environment name"
+variable "enabled" {
+  description = "Enable NFS Storage"
   type        = "string"
+}
+
+variable "environment" {
+  description = "Environment Name"
+  type        = "string"
+}
+
+variable "name" {
+  description = "Name of the NFS mount"
+  type        = "string"
+}
+
+variable "subnets" {
+  description = "Subnets to attach NFS to"
+  type        = "list"
+}
+
+variable "vpc_ids" {
+  description = "VPCs"
+  type        = "list"
 }
